c0ebfaaa1ffc9b6b01ff3000814cedbe889f33be
xiongchaohu <1732139715@qq.com>
2016-05-29 19:46:19 +0800

    add

---
+++ src/calculate-median.js
@@ -0,0 +1,22 @@
+function calculate_median(arr) {
+  //请勿改动函数名
+  
+	for(var i=0;i<arr.length;i++)
+	{
+		if((i+1)%2==0)
+		{
+			temp.push(arr[i]);
+		}
+	}
+	var n=arr.length;
+	var m=temp.length;
+	if(m%2==1)
+	{
+		return temp[Math.floor(m/2)];
+	}
+	else{
+		return (temp[m/2-1]+temp[m/2])/2;
+	}
+}
+
+module.exports = calculate_median;
